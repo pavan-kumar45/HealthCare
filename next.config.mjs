cea3cea7f4a2c630a8b478369e586431d5cdbadd
pavan-kumar45 <132326043+pavan-kumar45@users.noreply.github.com>
2024-08-20 12:17:11 +0530

    modified

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ export default withSentryConfig(withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "pavan-kumar-11",
+org: "pavan-kumar-ll",
 project: "care-pulse",
 
 // Only print logs for uploading source maps in CI
